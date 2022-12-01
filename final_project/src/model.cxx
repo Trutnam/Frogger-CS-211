47447e7b7fad53fc2d17d53f321080d51f3d0746
lilyr312 <104401508+lilyr312@users.noreply.github.com>
2022-12-01 13:40:05 -0600

    Update model.cxx

--- final_project/src/model.cxx
+++ final_project/src/model.cxx
@@ -1,16 +1,1 @@
-Model::lanes_ =
-        {
-                { 0.0f, "................................................................"}, //sidewalk lane 0
-                { 2.0f, "..xx.....xx....xx..xx........xx..xx..........xx......xx..xx.xx.."}, //car lane 1
-                {-4.0f, "...xxx..xxx........xxx....xxx.....xxx.....xxx....xxx..xxx...xxx."}, //bus lane 2
-                { 6.0f, "..xx..xx..xx...xx.....xx...xx..xx.....xx.....xx..xx....xx....xx."}, //car lane 3
-                {-4.0f, "....xx..xx....xx.....xx.xx.xx....xx...xx..xx...xx..xx....xx...xx"}, // car lane 4
-                { 2.0f, "xxx...xxx..xxx........xxx...xxx..xxx........xxx...xxx...xxx..xxx"}, // bus lane 5
-                { 0.0f, "................................................................"}, //sidewalk lane 4
-                {-2.0f, "xx..xx...x.x.xxx....xx..x...xxx.x.xx...xx..x..xx....x..xx..xxx.."}, //log lane 5
-                { 4.0f, "...xx.x..x.x...xxx..xx.x.x...xx...x.x..x.xx..x.x..xxx...xx..x..x"}, //log lane 6
-                {-6.0f, "..x.x..xxx..x.x.x.xxx.....xxx..xx....x..x..x.x...xxxxxx..x....x."}, // log lane 7
-                { 4.0f, "..xxxx...xx..x..x.x.x.x...x...x.xxxxx....xxx...x...x.x..xxx...x."}, // log lane 8
-                {-2.0f, "....xxx...xxx..x.x..x...x..xxxx...x..xx...x..xxxxx...x.x.xxx.x.."}, // log lane 9
-                { 0.0f, "xxxxxxx...xxxxxx...xxxxxxxxxxxx....xxxxxxxxx..xxxxxxxx..xxxxxxxx"}, //wall lane 10
-        };
+#include "model.hxx"
