96e999fd5b5acefc8b2291728dc8aecb1d60f953
lilyr312 <104401508+lilyr312@users.noreply.github.com>
2022-11-30 23:51:34 -0600

    Update view.hxx

--- Frogger/src/view.hxx
+++ Frogger/src/view.hxx
@@ -2,19 +2,26 @@
 
 #include "model.hxx"
 
+#include <string>
+
 class View
 {
 public:
     explicit View(Model const& model);
 
-    ge211::Dims<int> inial_window_dimensions() const;
+    ge211::Dims<int> initial_window_dimensions() const;
 
     void draw(ge211::Sprite_set& set);
 
 private:
     Model const& model_;
 
-    ge211::Rectangle_sprite ()
-
-
+    ge211::Rectangle_sprite() frog_sprite;
+    ge211::Rectangle_sprite() car_sprite;
+    ge211::Rectangle_sprite() bus_sprite;
+    ge211::Rectangle_sprite() log_sprite;
+    ge211::Rectangle_sprite() water_sprite;
+    ge211::Rectangle_sprite() street_sprite;
+    ge211::Rectangle_sprite() sidewalk_sprite;
+    ge211::Text_sprite() score_sprite;
 };
