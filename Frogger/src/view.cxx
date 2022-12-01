b216b6a30479f3d075808ea7b306288e854bd95d
lilyr312 <104401508+lilyr312@users.noreply.github.com>
2022-12-01 00:09:59 -0600

    Update view.cxx

--- Frogger/src/view.cxx
+++ Frogger/src/view.cxx
@@ -13,6 +13,8 @@ static Color const water_color {19, 178, 207};
 static Color const street_color {33, 33, 33};
 static Color const sidewalk_color {120, 120, 120};
 
+score = 0;
+
 View::View(Model const& model)
         : model_(model),
         frog_sprite({grid_size/2, grid_size/2}, frog_color),
@@ -22,7 +24,7 @@ View::View(Model const& model)
         water_sprite({grid_size, grid_size}, water_color),
         street_sprite({grid_size, grid_size}, street_color),
         sidewalk_sprite({grid_size, grid_size}, sidewalk_color)
-        score_sprite({})
+        score_sprite({string(score)})
 { }
 
 void
