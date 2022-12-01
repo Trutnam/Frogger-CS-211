6045a597927b4a9f57b5925086267c71064d5512
Trutnam <54719030+Trutnam@users.noreply.github.com>
2022-12-01 06:47:51 +0000

    reuploaded

--- final_project/src/view.cxx
+++ final_project/src/view.cxx
@@ -1,30 +1,7 @@
 #include "view.hxx"
 
-using Color = ge211::Color;
-using Sprite_set = ge211::Sprite_set;
-
-static int const grid_size = 36;
-
-static Color const frog_color {43, 156, 28};
-static Color const car_color {255, 30, 0};
-static Color const bus_color {0, 21, 255};
-static Color const log_color {74, 44, 19};
-static Color const water_color {19, 178, 207};
-static Color const street_color {33, 33, 33};
-static Color const sidewalk_color {120, 120, 120};
-
-score = 0;
-
 View::View(Model const& model)
-        : model_(model),
-        frog_sprite({grid_size/2, grid_size/2}, frog_color),
-        car_sprite({grid_size, grid_size}, car_color),
-        bus_sprite({grid_size, grid_size}, bus_color),
-        log_sprite({grid_size, grid_size}, log_color),
-        water_sprite({grid_size, grid_size}, water_color),
-        street_sprite({grid_size, grid_size}, street_color),
-        sidewalk_sprite({grid_size, grid_size}, sidewalk_color)
-        score_sprite({string(score)})
+        : model_(model)
 { }
 
 void
@@ -32,4 +9,3 @@ View::draw(ge211::Sprite_set& set)
 {
     // This needs to do something!
 }
-
--- final_project/src/view.hxx
+++ final_project/src/view.hxx
@@ -2,28 +2,19 @@
 
 #include "model.hxx"
 
-#include <string>
-
 class View
 {
 public:
     explicit View(Model const& model);
 
-    ge211::Dims<int> initial_window_dimensions() const;
+    ge211::Dims<int> inial_window_dimensions() const;
 
     void draw(ge211::Sprite_set& set);
-    
-    int score;
 
 private:
     Model const& model_;
 
-    ge211::Rectangle_sprite() frog_sprite;
-    ge211::Rectangle_sprite() car_sprite;
-    ge211::Rectangle_sprite() bus_sprite;
-    ge211::Rectangle_sprite() log_sprite;
-    ge211::Rectangle_sprite() water_sprite;
-    ge211::Rectangle_sprite() street_sprite;
-    ge211::Rectangle_sprite() sidewalk_sprite;
-    ge211::Text_sprite() score_sprite;
+    ge211::Rectangle_sprite ()
+
+
 };
