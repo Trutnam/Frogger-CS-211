b3621e4f60f9de17afb315bfca252931874d823b
lilyr312 <104401508+lilyr312@users.noreply.github.com>
2022-12-01 16:39:52 -0600

    reorder lanes, add a frog_position variable

--- final_project/src/model.hxx
+++ final_project/src/model.hxx
@@ -10,24 +10,37 @@ public:
     // get score function:
     char* get_score() const;
 
-
     std::vector<std::pair<float, std::string>> lanes_ =
             {
-                    { 0.0f, "................................................................"}, //sidewalk lane 0
-                    { 2.0f, "..xx.....xx....xx..xx........xx..xx..........xx......xx..xx.xx.."}, //car lane 1
-                    {-4.0f, "...xxx..xxx........xxx....xxx.....xxx.....xxx....xxx..xxx...xxx."}, //bus lane 2
-                    { 6.0f, "..xx..xx..xx...xx.....xx...xx..xx.....xx.....xx..xx....xx....xx."}, //car lane 3
-                    {-4.0f, "....xx..xx....xx.....xx.xx.xx....xx...xx..xx...xx..xx....xx...xx"}, // car lane 4
-                    { 2.0f, "xxx...xxx..xxx........xxx...xxx..xxx........xxx...xxx...xxx..xxx"}, // bus lane 5
-                    { 0.0f, "................................................................"}, //sidewalk lane 4
-                    {-2.0f, "xx..xx...x.x.xxx....xx..x...xxx.x.xx...xx..x..xx....x..xx..xxx.."}, //log lane 5
-                    { 4.0f, "...xx.x..x.x...xxx..xx.x.x...xx...x.x..x.xx..x.x..xxx...xx..x..x"}, //log lane 6
-                    {-6.0f, "..x.x..xxx..x.x.x.xxx.....xxx..xx....x..x..x.x...xxxxxx..x....x."}, // log lane 7
-                    { 4.0f, "..xxxx...xx..x..x.x.x.x...x...x.xxxxx....xxx...x...x.x..xxx...x."}, // log lane 8
-                    {-2.0f, "....xxx...xxx..x.x..x...x..xxxx...x..xx...x..xxxxx...x.x.xxx.x.."}, // log lane 9
-                    { 0.0f, "xxxxxxx...xxxxxx...xxxxxxxxxxxx....xxxxxxxxx..xxxxxxxx..xxxxxxxx"}, //wall lane 10
+                    { 0.0f, "xxx..xxx..xxxx.xx..xx.xx"}, //wall lane 0
+                    {-2.0f, "....xxx...xxx..x.x..x...xxxxxxxx...x..xxx..x."
+                            ".xxxxx...x.x.xxx.x.."}, // log lane 1
+                    { 4.0f, "..xxxx...xx..x..xxx.x.x...x...xxxxxxx....xxx...x"
+                            "...x.x..xxx...x."}, // log lane 2
+                    {-6.0f, "..x.x..xxx..x.x.xxxxx.....xxxxxxx....x..x..xxx.."
+                            ".xxxxxx..x....x."}, // log lane 3
+                    { 4.0f, "...xxxx..x.x...xxx..xxxxxx...xx...x.x..x.xx..x.x"
+                            "..xxx...xx..x..x"}, //log lane 4
+                    {-2.0f, "xx..xx...x.x.xxx....xx..x...xxx.x.xx...xx..x..xx"
+                            "....x..xx..xxx.."}, //log lane 5
+                    { 0.0f, "................................................"
+                            "................"}, //sidewalk lane 6
+                    { 2.0f, "xxx...xxx..xxx........xxx...xxx..xxx........xxx."
+                            "..xxx...xxx..xxx"}, // bus lane 7
+                    {-4.0f, "....xx..xx....xx.....xx.xx.xx....xx...xx..xx.."
+                            ".xx..xx....xx...xx"}, // car lane 8
+                    { 6.0f, "..xx..xx..xx...xx.....xx...xx..xx.....xx.....xx."
+                            ".xx....xx....xx."}, //car lane 9
+                    {-4.0f, "...xxx..xxx........xxx....xxx.....xxx.....xxx..."
+                            ".xxx..xxx...xxx."}, //bus lane 10
+                    { 2.0f, "..xx.....xx....xx..xx........xx..xx..........xx."
+                            ".....xx..xx.xx.."}, //car lane 11
+                    { 0.0f, "................................................"
+                            "................"}, //sidewalk lane 12
+
             };
     char* score_ = "Score: 0";
     bool alive_ = true;
+    ge211::geometry::Posn<int> frog_pos;
 
 };
