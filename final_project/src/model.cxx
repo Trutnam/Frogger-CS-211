ac608b901bba6ae2cd204f1a7c223b19435eedc4
Trutnam <54719030+Trutnam@users.noreply.github.com>
2022-12-02 07:00:41 +0000

    screen movement working! (positive velocity)

--- final_project/src/model.cxx
+++ final_project/src/model.cxx
@@ -12,19 +12,35 @@ std::vector<std::pair<float, std::string>> Model::get_lanes(){
 
 
 void Model::on_frame(double dt){
-    elapsed_time_ += dt;
+    elapsed_time_ = int(elapsed_time_ + 1);
     int initial_posn;
+    int amt_of_spots;
+    bool dir = true;
     // for each pair in lanes
-    for (int i = 0; i < lanes_.size(); i ++){
+    for (int i = 0; i < lanes_.size(); i ++) {
         // for each vector in a lane
-       for (int j = 0; j < lanes_[0].second.size(); j++) {
-           initial_posn = (int)(elapsed_time_ * lanes_[i].first) % 60;
-           if (initial_posn < 0){
-               initial_posn = 60 - (abs(initial_posn) % 60);
-           }
-           lanes_[i].second[j] = lanes_[i].second[(initial_posn + j) % 60];
+        for (int j = 0; j < lanes_[0].second.size(); j++) {
+            amt_of_spots = lanes_[i].second.size();
+            if (lanes_[i].first < 0){
+                dir = false;
+            }
+            else{
+                dir = true;
+            }
+            initial_posn =
+                    (int) (elapsed_time_ * lanes_[i].first) % amt_of_spots;
+            if (initial_posn < 0) {
+                initial_posn = amt_of_spots - (abs(initial_posn) %
+                                               amt_of_spots);
+            }
+            if (initial_posn == 12 && dir) {
 
-       }
+                lanes_[i].second[j] = lanes_[i].second[int(j + 1) %
+                                                       amt_of_spots];
+
+
+            }
+    }
     }
 
 }
--- final_project/src/model.hxx
+++ final_project/src/model.hxx
@@ -16,31 +16,46 @@ public:
 
     std::vector<std::pair<float, std::string>> lanes_ =
             {
-                    { 0.0f, "xxx..xxx..xxxx.xx..xx.xx"}, //wall lane 0
-                    {2.0f, "....xxx...xxx..x.x..x...xxxxxxxx...x..xxx..x."
-                            ".xxxxx...x.x.xxx.x.."}, // log lane 1
-                    { 2.0f, "..xxxx...xx..x..xxx.x.x...x...xxxxxxx....xxx...x"
-                            "...x.x..xxx...x."}, // log lane 2
-                    {2.0f, "..x.x..xxx..x.x.xxxxx.....xxxxxxx....x..x..xxx.."
-                            ".xxxxxx..x....x."}, // log lane 3
-                    { 2.0f, "...xxxx..x.x...xxx..xxxxxx...xx...x.x..x.xx..x.x"
-                            "..xxx...xx..x..x"}, //log lane 4
-                    {2.0f, "xx..xx...x.x.xxx....xx..x...xxx.x.xx...xx..x..xx"
-                            "....x..xx..xxx.."}, //log lane 5
-                    { 0.0f, "................................................"
-                            "................"}, //sidewalk lane 6
-                    { 3.0f, "xxx...xxx..xxx........xxx...xxx..xxx........xxx."
-                            "..xxx...xxx..xxx"}, // bus lane 7
-                    {-2.0f, "....xx..xx....xx.....xx.xx.xx....xx...xx..xx.."
-                            ".xx..xx....xx...xx"}, // car lane 8
-                    { 4.0f, "..xx..xx..xx...xx.....xx...xx..xx.....xx.....xx."
-                            ".xx....xx....xx."}, //car lane 9
-                    {-3.0f, "...xxx..xxx........xxx....xxx.....xxx.....xxx..."
-                            ".xxx..xxx...xxx."}, //bus lane 10
-                    { 2.0f, "..xx.....xx....xx..xx........xx..xx..........xx."
-                            ".....xx..xx.xx.."}, //car lane 11
-                    { 0.0f, "................................................"
-                            "................"}, //sidewalk lane 12
+                    {0.0f,"x.xx.xxx.xx.x..xxx.."}, //wall lane 0
+                    {2.0f,"x....xxxxxxxx....xxx"}, // log lane 1
+                    {3.0f,"xxx....xxxxx....xxxx"}, // log lane 2
+                    {2.0f, "x....xxxxx....xxxxx"}, // log lane 3
+                    {1.0f, "x....xxx...xxx....x"}, //log lane 4
+                    {2.0f, "x....xxxx.....xxxxx"}, //log lane 5
+                    { 0.0f,"...................."}, //sidewalk lane 6
+                    { 2.0f, ".........xx....xx..."}, // bus lane 7
+                    {1.0f, "..xx...........xx..."}, // car lane 8
+                    { 3.0f, "......xx...xx......."}, //car lane 9
+                    {2.0f, "..xx....xx.......xx."}, //bus lane 10
+                    { 1.0f, ".xx............xx..."}, //car lane 11
+                    { 0.0f, "...................."}, //sidewalk lane 12
+
+                 //   { 0.0f, "xxx..xxx..xxxx.xx..xx.xx"}, //wall lane 0
+                  //  {3.0f, "..........xxx..............xxxx............."
+                  //          ".xxxx........xxx...."}, // log lane 1
+                  //  { -3.0f, "..xxxx..........xxx...........xxxxxxx....xxx.
+                  //  ..."
+                  //          "........xxx....."}, // log lane 2
+                   // {1.0f, ".......xxx......xxxxx.....xxxxxxx.........xxxx.."
+                   //         ".xxxxxx........."}, // log lane 3
+                   // { -2.0f, "...xxx........xxx.....xxx......................"
+                   //         ".....xxx......"}, //log lane 4
+                   // {3.0f, "....xxx.......xxx..........xxxx........xxx......."
+                   //         ".......xxx..xxx.."}, //log lane 5
+                   // { 0.0f, "................................................"
+                    //        "................"}, //sidewalk lane 6
+                   // { 3.0f, "xxx........xxx........xxx........xxx........xxx."
+                   //         "..xxx........xxx"}, // bus lane 7
+                   // {-2.0f, "....xx..xx....xx...........xx....xx.......xx.."
+                   //         ".xx..xx....xx...xx"}, // car lane 8
+                   // { 4.0f, "..xx......xx..........xx.......xx.....xx.....xx."
+                   //         ".xx....xx....xx."}, //car lane 9
+                    //{-3.0f, "...xxx.............xxx............xxx.....xxx..."
+                    //        ".xxx..xxx...xxx."}, //bus lane 10
+                    //{ 2.0f, "..xx.....xx........xx............xx..........xx."
+                    //        ".....xx..xx.xx.."}, //car lane 11
+                    //{ 0.0f, "................................................"
+                    //        "................"}, //sidewalk lane 12
 
             };
     char* score_ = "Score: 0";
